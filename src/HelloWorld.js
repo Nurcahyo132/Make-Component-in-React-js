72e6b3d55a79f9fa4c8c1505ea620ebb3ff00d77
Nurcahyo132 <hartantonurcahyo@gmail.com>
2023-04-11 21:21:54 +0700

    Belajar React Pertama kali

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class HelloWorld extends React.Component{
+    render(){
+        return(
+            <div className="warna">Hello World</div>
+        );
+    }
+}
+
+export default HelloWorld;
